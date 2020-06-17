eedd158a3c6f00ec91d234410107cada3a03a6fd
Bernard Tison <bernard.tison@gmail.com>
2020-06-17 18:50:14 +0200

    Add basic health check

---
+++ src/main/java/com/redhat/erdemo/responder/rest/ApplicationHealthCheck.java
@@ -0,0 +1,19 @@
+package com.redhat.erdemo.responder.rest;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+import org.eclipse.microprofile.health.Readiness;
+
+@Liveness
+@Readiness
+@ApplicationScoped
+public class ApplicationHealthCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("Health check").up().build();
+    }
+}
